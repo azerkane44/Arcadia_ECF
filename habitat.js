6cf4dd0bcbb0ca7e6992b0aa541dbb2c37184a2a
Benchrif <140433811+azerkane44@users.noreply.github.com>
2024-10-12 15:47:29 +0200

    Add files via upload

---
+++ habitat.js
@@ -0,0 +1,8 @@
+function toggleDescription() {
+    var desc = document.getElementById("descc");
+    if (desc.style.display === "none" || desc.style.display === "") {
+        desc.style.display = "block";
+    } else {
+        desc.style.display = "none";
+    }
+}
